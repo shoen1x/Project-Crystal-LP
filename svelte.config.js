09837de6e620e09d80cced39a506f9d6d76b8f8d
shoen1x <mohdmuizz22@yahoo.com>
2025-06-03 22:01:53 +0800

    ci: :green_heart: Changed the build adapter from node to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 const config = {
 	preprocess: vitePreprocess(),
